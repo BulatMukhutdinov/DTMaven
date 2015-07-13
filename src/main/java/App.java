054df197e7bbafbb9f624a648e7d02be044d9fc0
BulatMukhutdinov <Bulatmm@yandex.ru>
2015-07-13 22:53:27 +0300

    Commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,21 @@
-/**
- * Created by Bulat on 13.07.2015.
- */
+import java.sql.*;
+
 public class App {
+    public static final String HOST = "jdbc:redshift://dt.ctueqkiqocnw.us-west-2.rds.amazonaws.com";
+    public static final String PORT = "5432";
+    private static final String URL = "DT";
+    private static final String USER = "DT";
+    private static final String PASS = "DoubleTrouble";
+
     public static void main(String args[]) {
-        System.out.println("Hello Maven!");
+        try {
+            Connection connection = DriverManager.getConnection(
+                    HOST + ":" + PORT + "/" + URL, USER,
+                    PASS);
+            System.out.println("Success!");
+        } catch (SQLException e) {
+            System.out.println("Connection Failed! Due to next reasons: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
